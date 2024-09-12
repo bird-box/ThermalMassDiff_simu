50485013571767c336aa5e85c1d75a24a98bfef3
bird-box <84137505+bird-box@users.noreply.github.com>
2024-09-12 16:55:56 +0900

    default_setting
    パラメータの設定

---
+++ LastInSIMU.py
@@ -0,0 +1,31 @@
+
+
+
+'''各種物性値'''
+
+E_com_In=0  
+''' Inの結合エネルギー [eV]'''
+SpeHeat_InN=0
+SpeHeat_GaN=0
+''' InN,GaNの比熱 [J/kg*K)'''
+ThermalCon_GaN=0
+''' InN,GaNの熱伝導率[w/mK]'''
+Density_InN=0
+Density_GaN=0
+''' InN,GaNの密度[g/cm3]'''
+
+
+'''シュミレーション設定'''
+
+grid_X=0
+grid_Y=0
+grid_X=0
+'''分割数(x,y,z)'''
+Dleng_X=0
+Dleng_Y=0
+Dleng_Z=0
+'''各軸方向のメッシュ間の距離[cm]'''
+DT=0
+'''微小時間[s]'''
+
+
